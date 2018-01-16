43bf28f8eb9f3af83c465e204a6688636ef4c77e
yknk <yknk@users.noreply.github.com>
2018-01-16 17:32:30 +0900

    Google Analyticsを修正

--- js/script.js
+++ js/script.js
@@ -144,7 +144,7 @@
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-110779746-1', 'honey.works');
+      ga('create', 'UA-112351279-1', 'honey.works');
       ga('set', 'anonymizeIp', true);
       ga('send', 'pageview');
     }
